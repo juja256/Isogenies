9e6ee30371ddd59bc0a722936e352911c59cc61f
Yevhen Hrubiian <grubian.euhen@gmail.com>
2018-10-24 00:10:04 +0300

    started work, add some old headers, trying adopt old Edwards interface.

---
+++ ec.h
@@ -0,0 +1,201 @@
+#ifndef EC_H
+#define EC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define NORMAL_POINT 1
+#define INFINITY_POINT 0
+
+/* 
+    In SIDH we use prime p = 2^372 * 3^239 - 1 and Field F_p^2 for 124bit quantum security leve
+    p = 3 (mod 4)
+*/
+#define BASE_FIELD_LEN (751/64 + 1 + 1) // 13 64bit words
+
+typedef unsigned long long u64;
+typedef unsigned u32;
+typedef unsigned char u8;
+
+typedef u64 GFElement[BASE_FIELD_LEN];
+
+
+/* For all x in F_p^2 x = a + i*b, where i is square root of QNR in F_p */
+typedef struct _GFex2Element {
+    GFElement a;
+    GFElement b;
+} GFex2Element;
+
+typedef u64 BigInt[BASE_FIELD_LEN]; 
+typedef u64 VeryBigInt[2*BASE_FIELD_LEN]; // up to 1280 bits
+
+typedef struct _EcPoint {
+    GFex2Element x;
+    GFex2Element y;
+} EcPoint;
+
+typedef struct _EcPointProj {
+    GFex2Element X;
+    GFex2Element Y;
+    GFex2Element Z;
+} EcPointProj;
+
+typedef int BOOL;
+#define TRUE 1
+#define FALSE 0
+
+typedef struct _Ec Ec;
+
+typedef void TGFAddFunc(const Ec*, const GFElement, const GFElement, GFElement);
+typedef void TGFMulFunc(const Ec*, const GFElement, const GFElement, GFElement);
+typedef void TGFSubFunc(const Ec*, const GFElement, const GFElement, GFElement);
+typedef void TGFSqrFunc(const Ec*, const GFElement, GFElement);
+
+typedef void TEcAdd(Ec*, const EcPointProj*, const EcPointProj*, EcPointProj*);
+typedef void TEcDouble(Ec*, const EcPointProj*, EcPointProj*);
+
+#define PRNG_STATE_LEN 256 
+typedef struct {
+    unsigned char state[PRNG_STATE_LEN];
+} PRNG;
+
+void PRNGInit(PRNG* generator, unsigned char* seed, int len);
+void PRNGRun(PRNG* generator); 
+void PRNGGenerateSequence(PRNG* generator, int bit_len, unsigned char* dest);
+
+/* Elliptic curve in Edwards:
+    x^2 + y^2 = 1 + d*x^2*y^2
+   or in Weierstrass form:
+    y^2 = x^3 + a*x + b 
+*/
+
+#define ED_192 0xE192
+#define ED_224 0xE224
+#define ED_256 0xE256
+#define ED_384 0xE384
+#define ED_521 0xE521
+#define ED_NOT_STANDARD 0xE000
+
+#define FIPS_192 0xF192
+#define FIPS_224 0xF224
+#define FIPS_256 0xF256
+#define FIPS_384 0xF384
+#define FIPS_521 0xF521
+#define FIPS_NOT_STANDARD 0xF000
+
+#define WINDOW_SIZE 4
+
+typedef struct _Ec {
+    GFElement d, a, b;
+    BOOL isEdwards;
+
+    u64 bitLen;
+    u64 wordLen;
+
+    BigInt n;
+    u64 cofactor;
+    EcPoint BasePoint;
+
+    BigInt p;
+    u64 curve_id;
+    
+    TGFMulFunc* GFMul;
+    TGFSqrFunc* GFSqr;
+
+    TEcAdd* EcAdd;
+    TEcDouble* EcDouble;
+
+    PRNG prng;
+
+    EcPointProj* T; // for precomputations
+} Ec;
+
+typedef Ec EcEd;
+typedef Ec EcW;
+
+
+int  EcEdInit(EcEd* ecc, u64 bitLen, const BigInt p, const EcPoint* bp, const BigInt n, const GFElement d);
+int  EcWInit(EcW* ecc, u64 bitLen, const BigInt p, const EcPoint* bp, const BigInt n, const GFElement a, const GFElement b);
+
+int  EcInitStandardCurve(Ec* ecc, u64 bitLen, BOOL isEdwards);
+
+void EcGenerateBasePoint(Ec* ecc, EcPoint* bp);
+
+int  EcCheckPointInMainSubGroup(Ec* ecc, const EcPoint* P);
+int  EcCheckPointOnCurve(Ec* ecc, const EcPoint* P);
+
+void EcDump(Ec* ecc, char* buf);
+
+void EcDestroy(Ec* ecc);
+
+/* Arithmetic on Elliptic Curves */
+int  EcPointCmp(Ec* ecc, const EcPoint* A, const EcPoint* B);
+void EcCopy(Ec* ecc, EcPoint* dest, const EcPoint* src);
+void EcCopyProj(Ec* ecc, EcPointProj* dest, const EcPointProj* src);
+
+void EcConvertAffineToProjective(Ec* ecc, const EcPoint* P, EcPointProj* Q);
+void EcConvertProjectiveToAffine(Ec* ecc, const EcPointProj* P, EcPoint* Q);
+
+void EcAdd(Ec* ecc, const EcPoint* A, const EcPoint* B, EcPoint* C);
+void EcDouble(Ec* ecc, const EcPoint* A, EcPoint* B);
+
+void EcAddProj(Ec* ecc, const EcPointProj* A, const EcPointProj* B, EcPointProj* C);
+void EcDoubleProj(Ec* ecc, const EcPointProj* A, EcPointProj* B);
+
+
+/* 
+Scalar Multiplications(all in the projective coordinates):
+    - AddAndDouble naive:
+    Result: Q = kP
+    Q <- 0P
+    H <- 1P
+    for i from 0 to m do
+        if k[i] = 1
+            Q <- Q + H
+        H <- 2H
+
+    - Montgomery constant-time:
+    Result: Q = kP
+    Q <- 0P
+    H <- 1P
+    for i from 0 to m do
+        if k[i] = 0
+            H <- H + Q
+            Q <- 2Q
+        else 
+            Q <- H + Q
+            H <- 2H 
+    - Windowed constant time for Edwards:
+    Precomputation: 0P, 1P, ... (2^w - 1)P
+    Result: Q = kP
+    Q <- 0P
+    for i from m/w downto 0 do
+        Q <- (2^w)Q
+        Q <- Q + k[i]P
+
+    - wNAF(not tested) 
+*/
+
+void EcScalarMulWindowedPrecomputation(Ec* ecc, const EcPoint* A, EcPointProj** T, int windowSize);
+void EcScalarMulWindowed(Ec* ecc, const EcPointProj* T, int windowSize, const BigInt k, EcPointProj* B);
+
+
+void EcScalarMulwNAFPrecomputation(Ec* ecc, const EcPoint* A, EcPointProj** T, int windowSize);
+void EcScalarMulwNAF(Ec* ecc, const EcPointProj* T, int windowSize, const BigInt k, EcPointProj* B);
+
+void EcScalarMulNaive(Ec* ecc, const EcPointProj* A, const BigInt k, EcPointProj* B);
+
+void EcScalarMulMontgomery(Ec* ecc, const EcPointProj* A, const BigInt k, EcPointProj* B);
+
+/* Fast windowed ScalarMul */
+void EcScalarMulByBasePoint(Ec* ecc, const BigInt k, EcPoint* B);
+
+/* Not so fast, but constant time Montgomery ladder */
+void EcScalarMul(Ec* ecc, const EcPoint * A, const BigInt k, EcPoint * B);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* EC_H */
---
+++ gf.h
@@ -0,0 +1,88 @@
+#ifndef GF_H
+#define GF_H
+
+#include "ec.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+extern const u64 unity[BASE_FIELD_LEN];
+extern const EcPoint uPEd;
+extern const EcPoint uPW;
+extern const EcPointProj uPPEd;
+extern const EcPointProj uPPW;
+extern const u64 p192[3];
+extern const u64 p224[4];
+extern const u64 p256[4];
+extern const u64 p384[6];
+extern const u64 p521[9];
+extern const u64 zero[2*BASE_FIELD_LEN];
+
+/* Common Arithmetics */
+
+void shr(u64 n, const u64* a, u64* res, u64 bits);
+#define div2(n, a) shr((n), (a), (a), 1)
+void shl(u64 n, const u64* a, u64* res, u64 bits);
+#define mul2(n, a) shl((n), (a), (a), 1)
+u64 get_bit(const u64* a, u64 num);
+void copy(u64* a, const u64* b, int len);
+u64 add(u64 n, const u64* a, const u64* b, u64* c);
+u64 sub(u64 n, const u64* a, const u64* b, u64* c);
+void _mul_raw(u64 a, u64 b, u64* low, u64* high);
+u64 _add_raw(u64 a, u64 b, u64* c);
+void mul_by_word(u64 n, const u64* a, u64 d, u64* c);
+void mul(u64 n, const u64* a, const u64* b, u64* c);
+void imul(u64 n, const u64* a, const u64* b, u64* c);
+void sqr(u64 n, const u64* a, u64* res);
+int word_bit_len(u64 w);
+int bigint_bit_len(u64 n, const u64* a);
+u64 sub_word(u64 n, const u64* a, const u64 b, u64* c);
+u64 add_word(u64 n, const u64* a, const u64 b, u64* c);
+
+void divide(u64 n, const u64* a, const u64* b, u64* quotient, u64* reminder);
+int cmp(u64 n, const u64* a, const u64* b);
+
+
+void add_mod(u64 n, const BigInt a, const BigInt b, const BigInt m, BigInt res);
+void mul_mod(u64 n, const BigInt a, const BigInt b, const BigInt m, BigInt res);
+void exp_mod(u64 n, const BigInt a, const BigInt b, const BigInt m, BigInt res);
+void inv_mod(u64 n, const BigInt a, const BigInt m, BigInt res);
+
+
+/* Galois' Fields Arithmetics  */
+void GFSqrt(const Ec* ecc, const GFElement a, GFElement r); // via Tonelli-Shanks
+void GFInitFromString(GFElement a, const char* str);
+void GFDump(const Ec* ecc, const GFElement a);
+void GFAdd(const Ec* ecc, const GFElement a, const GFElement b, GFElement c);
+void GFSub(const Ec* ecc, const GFElement a, const GFElement b, GFElement c);
+void GFNeg(const Ec* ecc, const GFElement a, GFElement c);
+void GFPow(const Ec* ecc, const GFElement a, const BigInt n, GFElement b);
+void GFInv(const Ec* ecc, const GFElement a, GFElement b);
+int  GFCmp(const Ec* ecc, const GFElement a, const GFElement b);
+void GFMul(const Ec* ecc, const GFElement a, const GFElement b, GFElement c);
+void GFSqr(const Ec* ecc, const GFElement a, GFElement c);
+void GFMulBy2Power(const Ec* ecc, const GFElement a, int pp, GFElement b);
+#define GFMulBy2(e, a, b) GFAdd(e, a, a, b);
+void GFMulByD(const EcEd* ecc, GFElement a);
+
+void GFMul_Cmn(const Ec* ecc, const GFElement a, const GFElement b, GFElement c);
+void GFMul_FIPS192(const Ec* ecc, const GFElement a, const GFElement b, GFElement c);
+void GFMul_FIPS224(const Ec* ecc, const GFElement a, const GFElement b, GFElement c);
+void GFMul_FIPS256(const Ec* ecc, const GFElement a, const GFElement b, GFElement c);
+void GFMul_FIPS384(const Ec* ecc, const GFElement a, const GFElement b, GFElement c);
+void GFMul_FIPS521(const Ec* ecc, const GFElement a, const GFElement b, GFElement c);
+
+void GFSqr_Cmn(const Ec* ecc, const GFElement a, GFElement c);
+void GFSqr_FIPS192(const Ec* ecc, const GFElement a, GFElement c);
+void GFSqr_FIPS224(const Ec* ecc, const GFElement a, GFElement c);
+void GFSqr_FIPS256(const Ec* ecc, const GFElement a, GFElement c);
+void GFSqr_FIPS384(const Ec* ecc, const GFElement a, GFElement c);
+void GFSqr_FIPS521(const Ec* ecc, const GFElement a, GFElement c);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GF_H */
